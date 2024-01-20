1770938ad8ea1bec05f8d1fe919afe79c381c8cc
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-20 11:13:18 +0200

    Added Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom';
+import Layout from './layouts/Layout';
+
 const App = () => {
-    return ();
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    
+                </Route>
+            </Routes>
+        </Router>
+    );
 };
 
 export default App;
---
+++ frontend/src/layouts/Layout.tsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom';
+
+const Layout = () => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <Hero />
+            <div className="container mx-auto py-10 flex-1">
+                <Outlet />
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default Layout;
