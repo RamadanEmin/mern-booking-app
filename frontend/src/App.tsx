3fe98c4b350e338f6e1d28e961ba52310d05f36b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-21 15:37:55 +0200

    Added Register page and styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,18 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Navigate,
 } from 'react-router-dom';
 import Layout from './layouts/Layout';
+import Register from './pages/Register';
 
 const App = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    
+                    <Route path="register" element={<Register />} />
+                    <Route path="*" element={<Navigate to="/" />} />
                 </Route>
             </Routes>
         </Router>
