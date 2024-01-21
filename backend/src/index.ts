d36f5b9d9d197eab308c2d87469a42adcd0b31dd
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-21 14:42:46 +0200

    Connect Mongodb and added register controller

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import mongoose from 'mongoose';
+import userRoutes from './routes/users';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use('/api/users', userRoutes);
 
 app.listen(7000, () => {
     console.log('Server running on localhost:7000');
