ff50346d18c34b5609872b732a72226b85bb590e
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-23 00:28:54 +0200

    Added Logout controller and update Header component

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -56,4 +56,15 @@ router.get('/validate-token', verifyToken, (req: Request, res: Response) => {
     res.status(200).send({ userId: req.userId })
 });
 
+router.post('/logout', (req: Request, res: Response) => {
+    res.cookie('auth_token', '', {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: process.env.NODE_ENV === 'production',
+        expires: new Date(0)
+    });
+
+    res.send();
+});
+
 export default router;
