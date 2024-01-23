def25abc5c97173bc8f774d2226d8e97a33afa7b
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-24 00:01:17 +0200

    Added Hotel model

---
+++ backend/src/models/hotel.ts
@@ -0,0 +1,62 @@
+import { Schema, model } from 'mongoose';
+
+const hotelSchema = new Schema({
+    userId: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    city: {
+        type: String,
+        required: true,
+    },
+    country: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    adultCount: {
+        type: Number,
+        required: true,
+    },
+    childCount: {
+        type: Number,
+        required: true,
+    },
+    facilities: [{
+        type: String,
+        required: true,
+    }],
+    pricePerNight: {
+        type: Number,
+        required: true,
+    },
+    starRating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    imageUrls: [{
+        type: String,
+        required: true,
+    }],
+    lastUpdated: {
+        type: Date,
+        required: true,
+    }
+});
+
+const Hotel = model('Hotel', hotelSchema);
+
+export default Hotel;
