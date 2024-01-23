4dcb461a0a962b5c660daad28282bf7ac17f7639
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-23 21:14:19 +0200

    Added Sign In test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -2,6 +2,24 @@ import { test, expect } from '@playwright/test';
 
 const UI_URL = "http://127.0.0.1:5173/";
 
+test('should allow the user to sign in', async ({ page }) => {
+    await page.goto(UI_URL);
+
+    await page.getByRole("link", { name: "Sign In" }).click();
+
+    await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+    await page.locator("[name=email]").fill("ramadan@abv.bg");
+    await page.locator("[name=password]").fill("pass1234");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByText("Sign in Successful!")).toBeVisible();
+    await expect(page.getByRole("link", { name: "My Bookings" })).toBeVisible();
+    await expect(page.getByRole("link", { name: "My Hotels" })).toBeVisible();
+    await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
+
 test("should allow user to register", async ({ page }) => {
     const testEmail = `test_register_${Math.floor(Math.random() * 90000) + 10000}@test.com`;
     await page.goto(UI_URL);
