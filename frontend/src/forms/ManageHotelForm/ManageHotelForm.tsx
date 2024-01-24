d132223b3254731ec9e68f2e97f5e82414d40718
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-24 23:27:35 +0200

    Added 'Manage hotel form' component

---
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -0,0 +1,42 @@
+import { FormProvider, useForm } from 'react-hook-form';
+
+export type HotelFormData = {
+    name: string;
+    city: string;
+    country: string;
+    description: string;
+    type: string;
+    pricePerNight: number;
+    starRating: number;
+    facilities: string[];
+    imageFiles: FileList;
+    imageUrls: string[];
+    adultCount: number;
+    childCount: number;
+};
+
+type Props = {
+    isLoading: boolean;
+};
+
+const ManageHotelForm = ({ isLoading }: Props) => {
+    const formMethods = useForm<HotelFormData>();
+
+    return (
+        <FormProvider {...formMethods}>
+            <form className="flex flex-col gap-10">
+                <span className="flex justify-end">
+                    <button
+                        disabled={isLoading}
+                        type='submit'
+                        className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl disabled:bg-gray-500"
+                    >
+                        {isLoading ? 'Saving...' : 'Save'}
+                    </button>
+                </span>
+            </form>
+        </FormProvider>
+    );
+};
+
+export default ManageHotelForm;
