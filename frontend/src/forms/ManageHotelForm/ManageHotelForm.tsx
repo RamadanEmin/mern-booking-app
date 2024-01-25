59f3d6c5ebbc57b9e9b57ddcd7a098096b455447
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-25 21:36:33 +0200

    Added Submit functionality to Add hotel page

--- frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
+++ frontend/src/forms/ManageHotelForm/ManageHotelForm.tsx
@@ -21,20 +21,52 @@ export type HotelFormData = {
 };
 
 type Props = {
+    onSave: (hotelFormData: FormData) => void;
     isLoading: boolean;
 };
 
-const ManageHotelForm = ({ isLoading }: Props) => {
+const ManageHotelForm = ({ onSave, isLoading }: Props) => {
     const formMethods = useForm<HotelFormData>();
+    const { handleSubmit } = formMethods;
+
+    const onSubmit = handleSubmit((formDataJson: HotelFormData) => {
+        const formData = new FormData();
+
+        formData.append('name', formDataJson.name);
+        formData.append('city', formDataJson.city);
+        formData.append('country', formDataJson.country);
+        formData.append('description', formDataJson.description);
+        formData.append('type', formDataJson.type);
+        formData.append('pricePerNight', formDataJson.pricePerNight.toString());
+        formData.append('starRating', formDataJson.starRating.toString());
+        formData.append('adultCount', formDataJson.adultCount.toString());
+        formData.append('childCount', formDataJson.childCount.toString());
+
+        formDataJson.facilities.forEach((facility, index) => {
+            formData.append(`facilities[${index}]`, facility);
+        });
+
+        if (formDataJson.imageUrls) {
+            formDataJson.imageUrls.forEach((url, index) => {
+                formData.append(`imageUrls[${index}]`, url);
+            });
+        }
+
+        Array.from(formDataJson.imageFiles).forEach((imageFile) => {
+            formData.append('imageFiles', imageFile);
+        });
+
+        onSave(formData);
+    });
 
     return (
         <FormProvider {...formMethods}>
-            <form className="flex flex-col gap-10">
-            <DetailsSection />
-            <TypeSection />
-            <FacilitiesSection />
-            <GuestsSection />
-            <ImageSection />
+            <form className="flex flex-col gap-10" onSubmit={onSubmit}>
+                <DetailsSection />
+                <TypeSection />
+                <FacilitiesSection />
+                <GuestsSection />
+                <ImageSection />
                 <span className="flex justify-end">
                     <button
                         disabled={isLoading}
