e4ab9ee14fafde45a9dc97cbdfb09aa81325cb4a
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-26 00:01:04 +0200

    Config sign In test

---
+++ e2e-tests/tests/manage-hotels.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+const UI_URL = "http://127.0.0.1:5173";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto(UI_URL);
+
+    await page.getByRole("link", { name: "Sign In" }).click();
+
+    await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+
+    await page.locator("[name=email]").fill("ramadan@abv.bg");
+    await page.locator("[name=password]").fill("pass1234");
+
+    await page.getByRole("button", { name: "Login" }).click();
+
+    await expect(page.getByText("Sign in Successful!")).toBeVisible();
+});
