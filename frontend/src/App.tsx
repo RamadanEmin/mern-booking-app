6bc7667caa2b7a89d6e7639e65e0f4961688019d
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-26 18:38:58 +0200

    Added My Hotels page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,28 @@ import {
     Route,
     Navigate,
 } from 'react-router-dom';
+import { useAppContext } from './contexts/AppContext';
 import Layout from './layouts/Layout';
 import Register from './pages/Register';
 import SignIn from './pages/SignIn';
+import AddHotel from './pages/AddHotel';
+import MyHotels from './pages/MyHotels';
 
 const App = () => {
+    const { isLoggedIn } = useAppContext();
+
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route path="register" element={<Register />} />
                     <Route path="sign-in" element={<SignIn />} />
+                    {isLoggedIn && (
+                        <>
+                            <Route path="add-hotel" element={<AddHotel />} />
+                            <Route path="my-hotels" element={<MyHotels />} />
+                        </>
+                    )}
                 </Route>
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
