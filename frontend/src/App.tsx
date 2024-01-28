7213a83a275a4505c25c09da9aed98bf2ded6ab1
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-28 11:11:04 +0200

    Added Search page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,8 @@ import Register from './pages/Register';
 import SignIn from './pages/SignIn';
 import AddHotel from './pages/AddHotel';
 import MyHotels from './pages/MyHotels';
+import EditHotel from './pages/EditHotel';
+import Search from './pages/Search';
 
 const App = () => {
     const { isLoggedIn } = useAppContext();
@@ -18,11 +20,13 @@ const App = () => {
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
+                    <Route path="search" element={<Search />} />
                     <Route path="register" element={<Register />} />
                     <Route path="sign-in" element={<SignIn />} />
                     {isLoggedIn && (
                         <>
                             <Route path="add-hotel" element={<AddHotel />} />
+                            <Route path="edit-hotel/:hotelId" element={<EditHotel />} />
                             <Route path="my-hotels" element={<MyHotels />} />
                         </>
                     )}
---
+++ frontend/src/pages/Search.tsx
@@ -0,0 +1,30 @@
+const Search = () => {
+
+    return (
+        <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+            <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+                <div className="space-y-5">
+                    <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
+                        Filter by:
+                    </h3>
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-5">
+                <div className="flex justify-between items-center">
+                    <select
+                        className="p-2 border rounded-md"
+                    >
+                        <option value="">Sort By</option>
+                        <option value="starRating">Star Rating</option>
+                        <option value="pricePerNightAsc">Price Per Night (low to high)</option>
+                        <option value="pricePerNightDesc">Price Per Night (high to low)</option>
+
+                    </select>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
