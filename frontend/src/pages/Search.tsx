102f37314e39cc9d0e1656e5fbbe434aa3c3703c
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-29 20:37:22 +0200

    Added Facilities Filter component

--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import StarRatingFilter from '../components/StarRatingFilter';
 import HotelTypesFilter from '../components/HotelTypesFilter';
+import FacilitiesFilter from '../components/FacilitiesFilter';
 
 const Search = () => {
     const [selectedStars, setSelectedStars] = useState<string[]>([]);
     const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
+    const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
 
     const handleStarsChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const startRating = event.target.value;
@@ -24,6 +26,15 @@ const Search = () => {
         );
     };
 
+    const handleFacilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const facility = event.target.value;
+
+        setSelectedFacilities((prevFacilities) => event.target.checked
+            ? [...prevFacilities, facility]
+            : prevFacilities.filter((prevFacility) => prevFacility !== facility)
+        );
+    };
+
     return (
         <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
             <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
@@ -33,6 +44,7 @@ const Search = () => {
                     </h3>
                     <StarRatingFilter selectedStar={selectedStars} onChange={handleStarsChange} />
                     <HotelTypesFilter selectedHotelTypes={selectedHotelTypes} onChange={handleHotelTypeChange} />
+                    <FacilitiesFilter selectedFacilities={selectedFacilities} onChange={handleFacilityChange} />
                 </div>
             </div>
 
