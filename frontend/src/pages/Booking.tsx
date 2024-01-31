81169ecaf3a7fd7b18367ffc5dd42cd3dee2a2c4
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-01-31 15:57:03 +0200

    Added Booking detail summary component

--- frontend/src/pages/Booking.tsx
+++ frontend/src/pages/Booking.tsx
@@ -1,9 +1,15 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import * as apiClient from '../api-client';
+import BookingDetailSummary from '../components/BookingDetailSummary';
+import { useSearchContext } from '../contexts/SearchContext';
+import { useState } from 'react';
 
 const Booking = () => {
+    const search = useSearchContext();
     const { hotelId } = useParams();
+
+    const [numberOfNights, setNumberOfNights] = useState<number>(0);
 
     const { data: hotel } = useQuery(
         'fetchHotelById',
@@ -19,7 +25,14 @@ const Booking = () => {
 
     return (
         <div className="grid md:grid-cols-[1fr_2fr]">
-            <BookingDetailSummary/>
+            <BookingDetailSummary
+                checkIn={search.checkIn}
+                checkOut={search.checkOut}
+                adultCount={search.adultCount}
+                childCount={search.childCount}
+                numberOfNights={numberOfNights}
+                hotel={hotel}
+            />
         </div>
     );
 };
