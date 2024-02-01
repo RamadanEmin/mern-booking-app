af4468062e47e9a4badd81d374f50b08df023f54
Ramadan Emin <113833640+RamadanEmin@users.noreply.github.com>
2024-02-01 22:53:47 +0200

    Update Booking form - added submit functionality

--- frontend/src/forms/BookingForm/BookingForm.tsx
+++ frontend/src/forms/BookingForm/BookingForm.tsx
@@ -1,3 +1,18 @@
+import { useForm } from 'react-hook-form';
+import { PaymentIntentResponse, UserType } from '../../../../backend/src/shared/types';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { StripeCardElement } from '@stripe/stripe-js';
+import { useSearchContext } from '../../contexts/SearchContext';
+import { useParams } from 'react-router-dom';
+import { useMutation } from 'react-query';
+import * as apiClient from '../../api-client';
+import { useAppContext } from '../../contexts/AppContext';
+
+type Props = {
+    currentUser: UserType;
+    paymentIntent: PaymentIntentResponse;
+};
+
 export type BookingFormData = {
     firstName: string;
     lastName: string;
@@ -11,10 +26,58 @@ export type BookingFormData = {
     paymentIntentId: string;
 };
 
-const BookingForm = () => {
+const BookingForm = ({ currentUser, paymentIntent }: Props) => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const search = useSearchContext();
+    const { hotelId } = useParams();
+
+    const { showToast } = useAppContext();
+
+    const { mutate: bookRoom, isLoading } = useMutation(apiClient.createRoomBooking, {
+        onSuccess: () => {
+            showToast({ message: 'Booking Saved!', type: 'SUCCESS' });
+        },
+        onError: () => {
+            showToast({ message: 'Error saving booking', type: 'ERROR' });
+        }
+    });
+
+    const { register, handleSubmit } = useForm<BookingFormData>({
+        defaultValues: {
+            firstName: currentUser.firstName,
+            lastName: currentUser.lastName,
+            email: currentUser.email,
+            adultCount: search.adultCount,
+            childCount: search.childCount,
+            checkIn: search.checkIn.toISOString(),
+            checkOut: search.checkOut.toISOString(),
+            hotelId: hotelId,
+            totalCost: paymentIntent.totalCost,
+            paymentIntentId: paymentIntent.paymentIntentId
+        }
+    });
+
+    const onSubmit = async (formData: BookingFormData) => {
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const result = await stripe?.confirmCardPayment(paymentIntent.clientSecret, {
+            payment_method: {
+                card: elements.getElement(CardElement) as StripeCardElement
+            }
+        });
+
+        if (result.paymentIntent?.status === 'succeeded') {
+            bookRoom({ ...formData, paymentIntentId: result.paymentIntent.id });
+        }
+    };
 
     return (
         <form
+            onSubmit={handleSubmit(onSubmit)}
             className="grid grid-cols-1 gap-5 rounded-lg border border-slate-300 p-5"
         >
             <span className="text-3xl font-bold">Confirm Your Details</span>
@@ -26,6 +89,7 @@ const BookingForm = () => {
                         className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal"
                         readOnly
                         disabled
+                        {...register('firstName')}
                     />
                 </label>
 
@@ -36,6 +100,7 @@ const BookingForm = () => {
                         className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal"
                         readOnly
                         disabled
+                        {...register('lastName')}
                     />
                 </label>
 
@@ -46,6 +111,7 @@ const BookingForm = () => {
                         className="mt-1 border rounded w-full py-2 px-3 text-gray-700 bg-gray-200 font-normal"
                         readOnly
                         disabled
+                        {...register('email')}
                     />
                 </label>
             </div>
@@ -55,7 +121,7 @@ const BookingForm = () => {
 
                 <div className="bg-blue-200 p-4 rounded-md">
                     <div className="font-semibold text-lg">
-                        Total Cost:
+                        Total Cost: €{paymentIntent.totalCost.toFixed(2)}
                     </div>
                     <div className="text-xs">Includes taxes and charges</div>
                 </div>
@@ -63,14 +129,19 @@ const BookingForm = () => {
 
             <div className="space-y-2">
                 <h3 className="text-xl font-semibold"> Payment Details</h3>
+                <CardElement
+                    id="payment-element"
+                    className="border rounded-md p-2 text-sm"
+                />
             </div>
 
             <div className="flex justify-end">
                 <button
+                    disabled={isLoading}
                     type="submit"
                     className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-md disabled:bg-gray-500"
                 >
-                    Confirm Booking
+                    {isLoading ? 'Saving...' : 'Confirm Booking'}
                 </button>
             </div>
         </form>
